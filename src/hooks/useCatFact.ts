599a75b2a7dbe7fb3e57df2ed7f151c6d4d6f882
Adrian Beckmann <adrian.beckmann@coduct.com>
2024-02-21 17:20:13 +0100

    Refactor useCatFact hook and Router tsx -> ts

--- src/hooks/useCatFact.ts
+++ src/hooks/useCatFact.ts
@@ -5,25 +5,21 @@ import { useFactHistoryStore } from "./useFactHistoryStore";
 export const useCatFact = () => {
 	const apiService = useContext(catFactApiServiceContext);
 	const { addFact, factHistory } = useFactHistoryStore();
-	const [currentFact, setCurrentFact] = useState(
-		factHistory[factHistory.length - 1],
-	);
 	const [loading, setLoading] = useState(false);
 
 	const getNewFact = useCallback(async () => {
 		setLoading(true);
 		const newFact = await apiService.fact();
-		setCurrentFact(newFact);
 		addFact(newFact);
 		setLoading(false);
 	}, [apiService, addFact]);
 
-	if (!loading && !currentFact) {
+	if (!loading && factHistory.length === 0) {
 		getNewFact();
 	}
 
 	return {
-		currentFact,
+		currentFact: factHistory[factHistory.length - 1],
 		getNewFact,
 		loading,
 	};

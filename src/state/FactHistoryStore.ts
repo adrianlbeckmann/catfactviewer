f9d0c687472d0b52cdd68cbb8d821391c60fb899
Adrian Beckmann <adrian.beckmann@coduct.com>
2024-02-21 17:28:08 +0100

    dont export unused

--- src/state/FactHistoryStore.ts
+++ src/state/FactHistoryStore.ts
@@ -5,7 +5,7 @@ import { FactHistory } from "../types/FactHistory";
 
 const FACT_HISTORY_STORAGE_KEY = "fact-history";
 
-export const factHistoryAtom = atomWithStorage<FactHistory>(
+const factHistoryAtom = atomWithStorage<FactHistory>(
 	FACT_HISTORY_STORAGE_KEY,
 	[],
 );
